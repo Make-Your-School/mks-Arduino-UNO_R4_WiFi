cccea45cbba60482fc8be4c38f63a8da7391ecab
s-light <git@s-light.eu>
2026-02-07 00:34:26 +0100

    tweak BLE example

--- examples/dev/UNO_R4_WiFi-Bluetooth_DroidPad/UNO_R4_WiFi-Bluetooth_DroidPad.ino
+++ examples/dev/UNO_R4_WiFi-Bluetooth_DroidPad/UNO_R4_WiFi-Bluetooth_DroidPad.ino
@@ -1,30 +1,12 @@
 // Extra benötigte Libraries:
 // - ArduinoBLE
 
-
-
-
-
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // This Example is Work-In-Progress
 // it currently does not work.
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #include <ArduinoBLE.h>
 
@@ -129,24 +111,29 @@ void connectPeripheral(BLEDevice peripheral) {
 }
 
 void handleDroidPadPeripheral() {
-    droidPadCharacteristic =
-        droidPadPeripheral.characteristic("dc3f5274-33ba-48de-8246-43bf8985b323");
+    droidPadCharacteristic = droidPadPeripheral.characteristic(
+        "dc3f5274-33ba-48de-8246-43bf8985b323");
 
     if (!droidPadCharacteristic) {
         Serial.println("DroidPad characteristic nicht gefunden!");
         droidPadPeripheral.disconnect();
         return;
     }
-    
+
     if (!droidPadCharacteristic.canRead()) {
         Serial.println("DroidPad characteristic nicht lesbar!");
-        // droidPadPeripheral.disconnect();
-        // return;
     }
     if (!droidPadCharacteristic.canWrite()) {
         Serial.println("DroidPad characteristic nicht schreibbar!");
-        // droidPadPeripheral.disconnect();
-        // return;
+    }
+    if (droidPadCharacteristic.canSubscribe()) {
+        Serial.println("DroidPad characteristic abonnierbar..");
+        if (!droidPadCharacteristic.subscribe()) {
+            Serial.println("subscription failed!");
+            droidPadPeripheral.disconnect();
+        } else {
+            // we have subscribed.
+        }
     }
 
     while (droidPadPeripheral.connected()) {
@@ -155,45 +142,27 @@ void handleDroidPadPeripheral() {
     Serial.println("Peripheral disconnected");
 }
 
-void exploreCharacteristic(BLECharacteristic characteristic) {
-    // print the UUID and properties of the characteristic
-    Serial.print("\tCharacteristic ");
-    Serial.print(characteristic.uuid());
-    Serial.print(", properties 0x");
-    Serial.print(characteristic.properties(), HEX);
-
-    // check if the characteristic is readable
-    if (characteristic.canRead()) {
-        // read the characteristic value
-        characteristic.read();
-
-        if (characteristic.valueLength() > 0) {
-            // print out the value of the characteristic
-            Serial.print(", value 0x");
-            printData(characteristic.value(), characteristic.valueLength());
-        }
-    }
-    Serial.println();
-
-    // loop the descriptors of the characteristic and explore each
-    for (int i = 0; i < characteristic.descriptorCount(); i++) {
-        BLEDescriptor descriptor = characteristic.descriptor(i);
-
-        exploreDescriptor(descriptor);
-    }
-}
-
 void handleDroidPadConnected() {
     // while the peripheral is connected
-    droidPadCharacteristic.read();
 
-    if (droidPadCharacteristic.valueLength() > 0) {
-        // print out the value of the droidPadCharacteristic
-        // Serial.print(", value 0x");
-        // Serial.print(String(droidPadCharacteristic.value()));
-        printData(droidPadCharacteristic.value(), droidPadCharacteristic.valueLength());
+    if (droidPadCharacteristic.valueUpdated()) {
+        Serial.print("value length: ");
+        Serial.print(droidPadCharacteristic.valueLength());
+        // droidPadCharacteristic.readValue(value);
+        printData(droidPadCharacteristic.value(),
+                  droidPadCharacteristic.valueLength());
     }
-    Serial.println();
+
+    // droidPadCharacteristic.read();
+
+    // if (droidPadCharacteristic.valueLength() > 0) {
+    //     // print out the value of the droidPadCharacteristic
+    //     // Serial.print(", value 0x");
+    //     // Serial.print(String(droidPadCharacteristic.value()));
+    //     printData(droidPadCharacteristic.value(),
+    //     droidPadCharacteristic.valueLength());
+    // }
+    // Serial.println();
     // // read the button pin
     // int buttonState = digitalRead(buttonPin);
 
